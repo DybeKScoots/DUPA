5056e2d63a8bff72fe28e928e499e4c8b84211f6
Joanna <chocatpaki@gmail.com>
2018-02-14 23:24:46 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,5 @@ var Header = Header || {};
 Header.cursorPosition = function cursorPosition(e) {
 	var cursorX = e.clientX,
 		cursorY = e.clientY;
-		alert(cursorX + ' , ' + cursorY);
+		
 };
-var h = new Header();
-h();
